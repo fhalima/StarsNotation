e747791f25316e034b4866fa323262af309ec41e
fhalima <ghazaloran2@gmail.com>
2021-03-19 16:19:30 +0100

    Initial commit

---
+++ assets/app.js
@@ -0,0 +1,52 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+require('jquery');
+
+//--------------------------------Gestion de la notation en étoiles -------------------------------//
+let firstconnect = 1;
+if (firstconnect === 1) {
+    SetStartsGold($("#note_form_value").val());
+    SetStartsGray(parseInt($("#note_form_value").val()) + 1);
+    firstconnect = 0;
+}
+for (let i = 1; i <= 5; i++) {
+    $("#star" + i).click(function () {
+        $("#note_form_value").val(i);
+        SetStartsGold(i);
+        SetStartsGray(i + 1);
+    });
+
+    let note_value = $("#note_form_value").val();
+
+    $("#star" + i).hover(function () {
+            SetStartsGold(i);
+            SetStartsGray(i + 1);
+        }, function () {
+            SetStartsGold(note_value);
+            SetStartsGray(parseInt(note_value) + 1);
+        }
+    );
+}
+//fonction de coloriage des etoiles de notation
+function SetStartsGold(i) {
+    $("#star" + i).css('color', "#ffdc0f");
+    if (i > 0)
+        SetStartsGold(i - 1);
+
+}
+
+function SetStartsGray(i) {
+    $("#star" + i).css('color', "#808080");
+    if (i < 5)
+        SetStartsGray(i + 1);
+
+}
+// start the Stimulus application
+// import './bootstrap';
---
+++ src/Entity/Note.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NoteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NoteRepository::class)
+ */
+class Note
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $value;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $comment;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateregister;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getDateregister(): ?\DateTimeInterface
+    {
+        return $this->dateregister;
+    }
+
+    public function setDateregister(\DateTimeInterface $dateregister): self
+    {
+        $this->dateregister = $dateregister;
+
+        return $this;
+    }
+}
